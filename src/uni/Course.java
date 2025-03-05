96161daf234fac7c848deaa67a0fceff42a20142
SaraBasiroo <sarabasiroo@yahoo.com>
2025-03-05 11:41:23 +0330

    Updated uni package

--- src/uni/Course.java
+++ src/uni/Course.java
@@ -1,4 +1,24 @@
 package uni;
 
+import java.util.ArrayList;
 public class Course {
+    public int courseID;
+    public String title;
+    public static ArrayList<Course> courseList = new ArrayList<Course>();
+    public int units;
+
+    public Course(String title, int units) {
+        this.title = title;
+        this.units = units;
+        courseList.add(this);
+        this.courseID = courseList.size();
+    }
+    public static Course findById(int id) {
+        for (Course course : courseList) {
+            if (course.courseID == id) {
+                return course;
+            }
+        }
+        return null;
+    }
 }
